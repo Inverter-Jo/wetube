6c6f7a805195eda8988276af3333454b629b429b
Inverter-Jo <58662375+Inverter-Jo@users.noreply.github.com>
2020-12-30 17:11:10 +0900

    seventh commit

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    return next();
+    });
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+
+
 
 export default app;
